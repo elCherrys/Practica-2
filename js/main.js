7e9d95c9a3f92cb9a135e9bfbef6cac1286d8919
Yahir Orozco 3 <yahir23ab@outlook.com>
2023-02-01 08:25:58 -0600

    Creation uwu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert ("Hello from js")
+function toggleTheme(){
+var theme = document.getElementsByTagName('link')[0];
+if (theme.getAttribute("href")=="css/light.css") {
+    theme.setAttribute("href", "css/dark.css");
+} else {
+    theme.setAttribute("href", "css/light.css");
+}
+}
